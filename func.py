cbcbc9470ab16bf5d20c8ad48cd5c7aa8a0f8fca
Lily Bevan <lilybevan31@gmail.com>
2025-01-28 09:32:05 +0000

    first working version!

--- func.py
+++ func.py
@@ -11,7 +11,7 @@ def get_dep_for_route(departure, first_wp, config):
     with open("departure_routes.txt", "r") as file:
         routes = file.readlines()
         for route in routes:
-            route_comp = route.strip.split(",")
+            route_comp = route.strip().split(",")
             if route_comp[0] == departure and route_comp[1] == config and route_comp[2] == first_wp:
                 return route_comp[3]
         return first_wp
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from func import *
 
 airport_alt = 136.0
 airport_icao = "EGPH"
+config = "24"
 facility = "GND"
 airport_app_data = "ILS24:55.9560884:-3.3546135:55.9438540:-3.3907010\nILS06:55.9437922:-3.3908724:55.9561296:-3.3544421\nILS:55.9513586:-3.3594437:118.0\nILS:55.9436373:-3.3341400:298.0"
 
@@ -35,12 +36,17 @@ while add_more_pilots:
         aircraft_cruise_fl = input("Enter cruise fl as an altitude (i.e. 33000): ")
         aircraft_destination = input("Enter aircraft destination airport: ")
         aircraft_rmk = input("Enter voice rules of a/c (v, r, t, or empty): ")
-        aircraft_route = get_route(airport_icao, aircraft_destination)
+        aircraft_route = str(get_route(airport_icao, aircraft_destination))
+        pseudoplane_route = f"{get_dep_for_route(airport_icao,aircraft_route.split()[1],config)} {aircraft_route.split()[2]} {aircraft_route.split()[3]}"
 
 
         pilot_list += (f"PSEUDOPILOT:{airport_icao}_M_{facility}\n@N:{aircraft_cs}:{aircraft_squawk.rjust(4,"0")}:1:{aircraft_latitude}:{aircraft_longitude}:{aircraft_altitude}:0:{aircraft_heading}:0:\n"
-                       f"$FP{aircraft_cs}:*A:{aircraft_rules}:{aircraft_type}/L:420:{airport_icao}:0000::{aircraft_cruise_fl}:{aircraft_destination}:00:00:0::/{aircraft_rmk}/:{aircraft_route}"
-                       f"")
+                       f"$FP{aircraft_cs}:*A:{aircraft_rules}:{aircraft_type}/L:420:{airport_icao}:0000::{aircraft_cruise_fl}:{aircraft_destination}:00:00:0::/{aircraft_rmk}/:{aircraft_route}\n"
+                       f"SIMDATA:{aircraft_cs}:*:*:25:1:0.010\n"
+                       f"$ROUTE:{pseudoplane_route}\n"
+                       f"DELAY:1:2\n"
+                       f"REQALT::7000\n"
+                       f"INITALPSEUDOPILOT:{airport_icao}_M_{facility}\n\n")
     else:
         add_more_pilots = False
         scenario_file_str += f"\n\n{pilot_list}"
