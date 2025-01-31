e032b98bb1f768b3cd935fb6e8a0e3e28d2b8e41
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-01-31 12:16:15 +0000

    remove theme

--- interface.py
+++ interface.py
@@ -18,7 +18,7 @@ class App(customtkinter.CTk):
         # Modes: "System" (standard), "Dark", "Light"
         customtkinter.set_appearance_mode("Dark")
         # Themes: "blue" (standard), "green", "dark-blue"
-        customtkinter.set_default_color_theme("theme.json")
+        customtkinter.set_default_color_theme("blue")
 
         # configure window
         self.title("Sweatbox Scenario Generator")
