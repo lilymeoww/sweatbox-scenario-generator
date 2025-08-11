11244ab000d0955becf3cfbdc5f32d376c17a9f9
lyraongithub <131717187+lyraongithub@users.noreply.github.com>
2025-08-11 16:46:26 +0100

    Fixed error with ownership of adep error airports.

--- utils.py
+++ utils.py
@@ -97,7 +97,7 @@ class Pilot:
         Pseudo route for aircraft
     """
 
-    def __init__(self, cs: str, lat: str, long: str, alt: str, hdg: str, dep: str, sq: str, rules: str, ac_type: str, crz: str, dest: str, rmk: str, rte: str, pseudo_route: str, speed: str = "420", timeUntilSpawn: str = "0", levelByFix: str = '', levelByLevel: str = "3000",owner: str = None):
+    def __init__(self, cs: str, lat: str, long: str, alt: str, hdg: str, dep: str, sq: str, rules: str, ac_type: str, crz: str, dest: str, rmk: str, rte: str, pseudo_route: str, *, speed: str = "420", timeUntilSpawn: str = "0", levelByFix: str = '', levelByLevel: str = "3000",owner: str = None):
         self.cs = cs
         self.lat = lat
         self.long = long
@@ -408,7 +408,7 @@ def generate_random_plans(amount: int, dep: Airport, vfr_factor: int, incorrect_
                 depAirport = random.choice(possAirports) #.split(",") # TODO: Determine why this was here.
 
         pilots.append(Pilot(cs, lat, long, dep.altitude, hdg,
-                            depAirport, sq, rules, acType, crz, dest, rmk, rte, ""))   
+                            depAirport, sq, rules, acType, crz, dest, rmk, rte, "", owner=dep.icao))   
 
     return pilots, occupiedStands
 
